2b98183d023b743fdcc85a32a46db78cf987cfac
Renegade <singharpit.asb@gmail.com>
2026-02-13 09:12:17 +0530

    feat: implement login and token verification logic

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
+const ApiError = require("../utils/ApiError");
+
+const generateToken = (userId) =>{
+    return jwt.sign({id:userId}, process.env.JWT_SECRET,{
+        expiresIn:"1d",}    
+);
+};
+
+exports.login = asyncHandler(async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        throw new ApiError(400, "email and password are required");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new ApiError(401, "credentials are invalid");
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        throw new ApiError(401, "credentials are invalid")
+    }
+
+    const token = generateToken(user._id);
+
+    
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
+
+exports.verifytoken = asynchandler(async(req, res)=>{
+      res.status(200).json({
+    status: "success",
+    message: "Token is valid",
+  });
+})
+
+     
